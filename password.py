02fdf8950a2fcdbc44445e1e124f61f9f9409d41
BlackShit <103965764+BrightShit@users.noreply.github.com>
2022-05-29 23:27:28 +0300

    Update password.py

--- password.py
+++ password.py
@@ -2,25 +2,23 @@
 from random import randint
 import random
 import string
-string.ascii_letters
-string.ascii_lowercase
-string.ascii_uppercase
+
+from numpy import pad
 def password():
-    letter1 = random.choice('qwertyuiopasdfghjklzxcvbnm')
-    letter2 = random.choice('QWERTYUIOPASDFGHJKLZXCVBNM')
-    letter3 = random.choice('qwertyuiopasdfghjklzxcvbnm')
-    letter4 = random.choice('qwertyuiopasdfghjklzxcvbnm')
-    letter5 = random.choice('qwertyuiopasdfghjklzxcvbnm')
-    Password1=random.choice('1234567890')
-    Password2=random.choice('1234567890')
-    Password3=random.choice('1234567890')
-    Password4=random.choice('1234567890')
-    Password5=random.choice('1234567890')
-    special=random.choice('!@#$%^&*()_+=-')
+    letter1 = random.choice('qwertyuiopasdfghjklzxcvbnm') #generate the 1 letter
+    letter2 = random.choice('QWERTYUIOPASDFGHJKLZXCVBNM') #generate the 2 letter
+    letter3 = random.choice('qwertyuiopasdfghjklzxcvbnm') #Generates the 3 letter
+    letter4 = random.choice('qwertyuiopasdfghjklzxcvbnm') #Generates the 4 letter
+    letter5 = random.choice('qwertyuiopasdfghjklzxcvbnm') #Generates the 5 letter
+    Password1=random.choice('1234567890') #Generatess random number between 0 to 9
+    Password2=random.choice('1234567890') #Generatess random number between 0 to 9
+    Password3=random.choice('1234567890') #Generatess random number between 0 to 9
+    Password4=random.choice('1234567890') #Generatess random number between 0 to 9
+    Password5=random.choice('1234567890') #Generatess random number between 0 to 9
+    special=random.choice('!@#$%^&*()_+=-') #Generatess random special characters
     all = letter1 + letter2 + letter3 + letter4 + letter5 + Password1 + Password2 + Password3 + Password4 + Password5 + special
     print(all)
     file=open("C:/Users/nitay\Desktop/PASSWORD.txt", "w")
     file.write(all)
     file.close
 password()
-
