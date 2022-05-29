6c57c578fea3846824981d7eff75c464538b1240
BlackShit <103965764+BrightShit@users.noreply.github.com>
2022-05-29 23:19:49 +0300

    Add files via upload

---
+++ password.py
@@ -0,0 +1,30 @@
+from logging import root
+from random import randint
+import tkinter as tk
+import random
+from tkinter import Canvas, filedialog, Text
+import string
+
+from numpy import pad
+string.ascii_letters
+string.ascii_lowercase
+string.ascii_uppercase
+def password():
+    letter1 = random.choice('qwertyuiopasdfghjklzxcvbnm')
+    letter2 = random.choice('QWERTYUIOPASDFGHJKLZXCVBNM')
+    letter3 = random.choice('qwertyuiopasdfghjklzxcvbnm')
+    letter4 = random.choice('qwertyuiopasdfghjklzxcvbnm')
+    letter5 = random.choice('qwertyuiopasdfghjklzxcvbnm')
+    Password1=random.choice('1234567890')
+    Password2=random.choice('1234567890')
+    Password3=random.choice('1234567890')
+    Password4=random.choice('1234567890')
+    Password5=random.choice('1234567890')
+    special=random.choice('!@#$%^&*()_+=-')
+    all = letter1 + letter2 + letter3 + letter4 + letter5 + Password1 + Password2 + Password3 + Password4 + Password5 + special
+    print(all)
+    file=open("C:/Users/nitay\Desktop/PASSWORD.txt", "w")
+    file.write(all)
+    file.close
+password()
+
