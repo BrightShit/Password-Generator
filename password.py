3381b74f18864821e75f13699cec70b54f643a3d
BlackShit <103965764+BrightShit@users.noreply.github.com>
2022-06-18 18:09:05 +0300

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,16 @@
 
 import random
-password={}
-for i in range(0,10):
-    random__ = random.choice("1234567890qwertyuiopasdfghjklzxcvnm!@#$%^&*()QWERTYUIOPASDFGHJKLZXFGHM")
-    password[str(i)] = random__
+def generate():
+    password={}
+    for i in range(2,14):
+        random__ = random.choice("1234567890qwertyuiopASDFGHJKLzxcvbnm!@#$%^&*()")
+        password[str(i)] = random__
 
-new__lst=(str(password)[1:-1])
-new__lst=new__lst.replace('\'','').replace(',','').replace(":",'').replace(' ','')
-print("New password:",new__lst)
-input()
+    #new__lst=(str(password)[1:-1])
+    new__lst = (str(password)[1:-1])
+    new__lst=new__lst.replace('\'','').replace(',','').replace(":",'').replace(' ','')
+    new__lst = new__lst[:len(new__lst)//2]
+    print("New password:",new__lst)
+    input()
+
+generate()
