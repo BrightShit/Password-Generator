2b0b97720fb07f72dd548d31330f42441b7ecaf5
BlackShit <103965764+BrightShit@users.noreply.github.com>
2022-06-27 19:24:36 +0300

    Update password.py

--- password.py
+++ password.py
@@ -1,16 +1,34 @@
-
+from tkinter import *
 import random
-def generate():
-    password={}
-    for i in range(2,14):
-        random__ = random.choice("1234567890qwertyuiopASDFGHJKLzxcvbnm!@#$%^&*()")
-        password[str(i)] = random__
+lst = []
+window = Tk()
+window.geometry("300x250")
+window.configure(bg="#347aeb")
+# Generating a random password
+window.title("Password generator")
+def generate_password():
+    generate_button['state'] = 'disabled'
+    for i in range(0,20):
+        lst.insert(i,random.choice("1234567890qwertyuiopASDFGHJKLzxcvbnm!@#$%^&*()"))
+    E = Label(window,width=35,borderwidth=5,text=lst)
+    E.pack()
+    save_button.pack()
+def quit():
+    window.quit()
 
-    #new__lst=(str(password)[1:-1])
-    new__lst = (str(password)[1:-1])
-    new__lst=new__lst.replace('\'','').replace(',','').replace(":",'').replace(' ','')
-    new__lst = new__lst[:len(new__lst)//2]
-    print("New password:",new__lst)
-    input()
 
-generate()
+def save():
+    file = open("Password.txt","w")
+    for letter in lst:
+        file.write(str(letter))
+    file.close()
+    save_label=Label(window,text="Password Saved")
+    save_label.pack()
+
+
+generate_button=Button(window,text="Click here to generate a password",padx=20,pady=10,command=generate_password)
+quit_button = Button(window,text="Quit",command=quit)
+save_button=Button(window,text="Save Password",command=save)
+generate_button.pack()
+quit_button.pack()
+window.mainloop()
